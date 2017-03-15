6d3040f4ffb736da098ae07245a888cae07e7cb7
Sam Lim <limsammy1@gmail.com>
2017-03-15 13:35:35 -0600

    insertion sort implementation

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,20 +1,25 @@
-require 'pry'
-
 class InsertionSort
 	def sort(list)
-		list_size = list.length
-		swapped = true
-		sorted_list = []
+		sorted_list = [list[0]]
+		list.delete_at(0)
 
-		while swapped do
-			swapped = false
-			(list_size-1).times do |i|
-				sorted_list.unshift(list[i])
-				binding.pry
+		list.each do |i|
+			index_counter = 0
+			while index_counter < sorted_list.length
+				if(i <= sorted_list[index_counter])
+					sorted_list.insert(index_counter, i)
+					break
+				elsif(index_counter == sorted_list.length-1)
+					sorted_list.insert(index_counter+1, i)
+					break
+				end
+				index_counter += 1
 			end
 		end
+		puts "Sorted list: #{sorted_list}"
 	end
 end
+
 
 insertion = InsertionSort.new
 insertion.sort([0, 3, 8, 5, 4, 9, 22])
