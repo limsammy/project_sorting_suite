cd8d70bf4211d56936220441d117f54ef6692df5
Sam Lim <limsammy1@gmail.com>
2017-03-16 12:07:20 -0600

    Sorts correctly but final array has each element within another array

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,9 +3,24 @@ require 'enumerator'
 
 class MergeSort
   def sort(list)
+    swapped = true
+    # final = []
+
     slice_count = list.size.to_i.floor
     chunked_list = list.each_slice(slice_count).to_a.each{ |element| element.fill nil, slice_count, 0 }.transpose.map(&:compact)
-    
+
+    while swapped do
+      swapped = false
+
+      (slice_count-1).times do |i|
+        if chunked_list[i][0] > chunked_list[i+1][0]
+          chunked_list[i], chunked_list[i+1] = chunked_list[i+1], chunked_list[i]
+          swapped = true
+        end
+      end
+      break if !swapped
+    end
+    puts "Sorted list: #{chunked_list}"
   end
 end
 
