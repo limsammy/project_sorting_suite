44ee453f128e78b09f0aaa2b1f13a6d66e83fb9c
Sam Lim <limsammy1@gmail.com>
2017-03-16 14:43:06 -0600

    Realized I was using bubble sort, rewriting everything

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,30 +1,25 @@
-# require 'pry'
+require 'pry'
 
 class MergeSort
 
   def sort(list)
-    swapped = true
-    sorted_list = []
-
-    slice_count = list.size.to_i
-    chunked_list = list.each_slice(slice_count).to_a.each{ |element| element.fill nil, slice_count, 0 }.transpose.map(&:compact)
-
-    while swapped do
-      swapped = false
-
-      (slice_count-1).times do |i|
-        if chunked_list[i][0] > chunked_list[i+1][0]
-          chunked_list[i], chunked_list[i+1] = chunked_list[i+1], chunked_list[i]
-          swapped = true
-        end
-      end
-      break if !swapped
-    end
-
-    (slice_count-1).times do |i|
-      sorted_list.push(chunked_list[i][0])
-    end
-# binding.pry
-    puts "Sorted list (merge): #{sorted_list}"
+    return list if list.size == 1
+    middle = list.size / 2
   end
+
+  def merge(left, right)
+    sorted_list = []
+    if left.empty?
+      right
+    elsif right.empty?
+      left
+    elsif left.first < right.first
+      [left.first] + merge(left[1..left.size], right)
+    else
+      [right.first] + merge(left, right[1..right.size])
+  end
+  
 end
+
+test = MergeSort.new
+test.sort([0, 3, 8, 5, 4, 9, 22])
